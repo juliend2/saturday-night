6a54e459700bb60684f8618732cb518eea5584d2
Julien Desrosiers <juliend2@gmail.com>
2020-02-19 20:36:11 -0500

    Initial commit: make things crazy enough

---
+++ saturday-night.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Plugin Name: Saturday Night
+ * Description: A plugin for party animal blog authors.
+ * Author: Julien Desrosiers
+ * License: GPLv2
+ */
+
+
+defined( 'ABSPATH' ) or die( 'I got your IP, and I just called the cops.' );
+
+
+// Enqueue the stylesheet:
+function sn_styles() {
+    $dir = plugin_dir_url(__FILE__);
+    wp_enqueue_style('saturday-night-style', $dir . '/styles.css', [], '0.1.0', 'all');
+}
+add_action('wp_enqueue_scripts', 'sn_styles');
+
+
+function sn_rainbow($atts, $content=null) {
+    return "<span class='sn-sc sn-rainbow'><span class='sn-rainbow-inner'>".$content."</span></span>";
+}
+add_shortcode( 'rainbow', 'sn_rainbow' );
+
+
+function sn_boom($atts, $content=null) {
+    return "<span class='sn-sc sn-boom'><span class='sn-boom-inner'>".$content."</span></span>";
+}
+add_shortcode( 'boom', 'sn_boom' );
+
+function sn_party($atts, $content=null) {
+    return "<span class='sn-sc sn-party'><span class='sn-party-inner'>".$content."</span></span>";
+}
+add_shortcode( 'party', 'sn_party' );
