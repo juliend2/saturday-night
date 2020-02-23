6883d13358e51514c0ad2bd1fcb0befc784f2853
artistejd <artistejd@timberwolf.dreamhost.com>
2020-02-23 13:42:13 -0800

    add [fire] and change namespace to match WP requirements

--- saturday-night.php
+++ saturday-night.php
@@ -12,31 +12,36 @@ defined( 'ABSPATH' ) or die( 'I got your IP, and I just called the cops.' );
 
 
 // Enqueue the stylesheet:
-function sn_styles() {
+function jdsn_styles() {
     $dir = plugin_dir_url(__FILE__);
     wp_enqueue_style('saturday-night-style', $dir . '/styles.css', [], '0.1.0', 'all');
 }
-add_action('wp_enqueue_scripts', 'sn_styles');
+add_action('wp_enqueue_scripts', 'jdsn_styles');
 
 
-function sn_rainbow($atts, $content=null) {
-    return "<span class='sn-sc sn-rainbow'><span class='sn-rainbow-inner'>".$content."</span></span>";
+function jdsn_rainbow($atts, $content=null) {
+    return "<span class='jdsn-sc jdsn-rainbow'><span class='jdsn-rainbow-inner'>".$content."</span></span>";
 }
-add_shortcode( 'rainbow', 'sn_rainbow' );
+add_shortcode( 'rainbow', 'jdsn_rainbow' );
 
 
-function sn_boom($atts, $content=null) {
-    return "<span class='sn-sc sn-boom'><span class='sn-boom-inner'>".$content."</span></span>";
+function jdsn_boom($atts, $content=null) {
+    return "<span class='jdsn-sc jdsn-boom'><span class='jdsn-boom-inner'>".$content."</span></span>";
 }
-add_shortcode( 'boom', 'sn_boom' );
+add_shortcode( 'boom', 'jdsn_boom' );
 
 
-function sn_party($atts, $content=null) {
-    return "<span class='sn-sc sn-party'><span class='sn-party-inner'>".$content."</span></span>";
+function jdsn_party($atts, $content=null) {
+    return "<span class='jdsn-sc jdsn-party'><span class='jdsn-party-inner'>".$content."</span></span>";
 }
-add_shortcode( 'party', 'sn_party' );
+add_shortcode( 'party', 'jdsn_party' );
 
-function sn_shit($atts, $content=null) {
-    return "<span class='sn-sc sn-shit'><span class='sn-shit-inner'>".$content."</span></span>";
+function jdsn_fire($atts, $content=null) {
+    return "<span class='jdsn-sc jdsn-fire'><span class='jdsn-fire-inner'>".$content."</span></span>";
 }
-add_shortcode( 'shit', 'sn_shit' );
+add_shortcode( 'fire', 'jdsn_fire' );
+
+function jdsn_shit($atts, $content=null) {
+    return "<span class='jdsn-sc jdsn-shit'><span class='jdsn-shit-inner'>".$content."</span></span>";
+}
+add_shortcode( 'shit', 'jdsn_shit' );
